7a2f46c3873445604fc2863f29c6247bd4235e94
Matthew Foyle <mfoyle93@gmail.com>
2023-09-11 15:40:30 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { DynamicWidget, DynamicContextProvider } from "@dynamic-labs/sdk-react";
+
 function App() {
-  return (
+  
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DynamicContextProvider
+        settings={{
+            environmentId: "8b5038f9-171c-480b-9f3c-294d868aeb74"
+        }}
+        
+      >
+        <DynamicWidget />
+      </DynamicContextProvider>
     </div>
   );
 }
