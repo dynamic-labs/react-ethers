37876e22914baff7e1c7dec092b3ab7c5d06e6d7
Matthew Foyle <mfoyle93@gmail.com>
2023-10-23 20:10:34 +0200

    feat: v19

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import './App.css';
+import "./App.css";
 
-import { DynamicWidget, DynamicContextProvider } from "@dynamic-labs/sdk-react";
+import {
+  DynamicWidget,
+  DynamicContextProvider,
+} from "@dynamic-labs/sdk-react-core";
+import { EthersExtension } from "@dynamic-labs/ethers-v5";
+
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { AlgorandWalletConnectors } from "@dynamic-labs/algorand";
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
+import { FlowWalletConnectors } from "@dynamic-labs/flow";
+import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
+import { CosmosWalletConnectors } from "@dynamic-labs/cosmos";
+import { MagicWalletConnectors } from "@dynamic-labs/magic";
+import { BloctoEvmWalletConnectors } from "@dynamic-labs/blocto";
 
 function App() {
-  
-return (
+  return (
     <div className="App">
       <DynamicContextProvider
         settings={{
-            environmentId: "8b5038f9-171c-480b-9f3c-294d868aeb74"
+          environmentId: "8b5038f9-171c-480b-9f3c-294d868aeb74",
+          walletConnectorExtensions: [EthersExtension],
+          walletConnectors: [
+            EthereumWalletConnectors,
+            AlgorandWalletConnectors,
+            SolanaWalletConnectors,
+            FlowWalletConnectors,
+            StarknetWalletConnectors,
+            CosmosWalletConnectors,
+            MagicWalletConnectors,
+            BloctoEvmWalletConnectors,
+          ],
         }}
-        
       >
         <DynamicWidget />
       </DynamicContextProvider>
