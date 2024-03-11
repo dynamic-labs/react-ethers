dd564b6882c4470e6e56bb2ecbd103b332ea5461
Matthew Foyle <mfoyle93@gmail.com>
2024-03-11 08:19:14 +0100

    fix: packages

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ import {
 import { EthersExtension } from "@dynamic-labs/ethers-v5";
 
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
-import { AlgorandWalletConnectors } from "@dynamic-labs/algorand";
-import { SolanaWalletConnectors } from "@dynamic-labs/solana";
-import { FlowWalletConnectors } from "@dynamic-labs/flow";
-import { StarknetWalletConnectors } from "@dynamic-labs/starknet";
-import { CosmosWalletConnectors } from "@dynamic-labs/cosmos";
-import { MagicWalletConnectors } from "@dynamic-labs/magic";
-import { BloctoEvmWalletConnectors } from "@dynamic-labs/blocto-evm";
 
 function App() {
   return (
@@ -22,16 +15,7 @@ function App() {
         settings={{
           environmentId: "2762a57b-faa4-41ce-9f16-abff9300e2c9",
           walletConnectorExtensions: [EthersExtension],
-          walletConnectors: [
-            EthereumWalletConnectors,
-            AlgorandWalletConnectors,
-            SolanaWalletConnectors,
-            FlowWalletConnectors,
-            StarknetWalletConnectors,
-            CosmosWalletConnectors,
-            MagicWalletConnectors,
-            BloctoEvmWalletConnectors,
-          ],
+          walletConnectors: [EthereumWalletConnectors],
         }}
       >
         <DynamicWidget />
